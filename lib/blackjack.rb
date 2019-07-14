b6ee547377e2ddfc47c7f70672c1d40ffdb91305
Lisa LaRochelle <larochelle.lisa@gmail.com>
2019-07-14 21:12:09 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -35,6 +35,9 @@ def hit?(current_card_total)
     current_card_total
   elsif user_input = 'h'
     current_card_total += deal_card
+  else invalid_command
+    prompt_user
+  end
 end
 
 def invalid_command
